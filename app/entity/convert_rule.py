fec1b659851169ba3d21befdd42774dc04e240c9
lordhasyim <lordhasyim@gmail.com>
2019-08-06 19:51:18 +0700

    ada yg typo, maklum si om kurang tidur

--- app/entity/convert_rule.py
+++ app/entity/convert_rule.py
@@ -2,6 +2,7 @@ from app import db
 from sqlalchemy.dialects.postgresql import UUID
 
 class ConvertRule(db.Model):
+	__tablename__ = "convert_rule"
 	id = db.Column(UUID(as_uuid=True), unique=True, primary_key=True)
 	default_amount = db.Column(db.Integer, nullable=False)
 	status = db.Column(db.String(50), nullable=False)
